b2ba0121a5c83f7901cee6bbdeba7b8c9938e5be
Sihan Zhao <zhaosihan@growingio.com>
2019-12-02 16:18:24 +0800

    refine calendar label style

--- packages/components/date-picker/common/Label/index.tsx
+++ packages/components/date-picker/common/Label/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Icon from '@gio-design/icon';
+import classnames from 'classnames';
 import './index.less';
 
 interface  Props {
@@ -16,9 +17,9 @@ const Label = (props: Props) => (
         onClick={props.onClick}
         type='button'
     >
-        <Icon className='icon calendar' type='calendar' fill='#7F7583' />
+        <Icon className={classnames('icon', 'calendar')} type='calendar' fill='#7F7583' />
         {props.label && (<span className='text' title={props.label}>{props.label}</span>)}
-        <Icon className={`icon arrow_down ${props.focus ? 'focus' : ''}`} type='down' fill='#666' />
+        <Icon className={classnames('icon', 'arrow_down', {'focus' : props.focus})} type='down' fill='#666' />
     </button>
 );
 
